02d90aaff3fa0e74631db1fd204c0de3f0f2464e
sobertrauter <robert_sauter@gmx.de>
2022-05-25 23:21:46 +0200

    special mode added

--- components/DiceWrapper.js
+++ components/DiceWrapper.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Dice from './Dice';
+import Dice from './dice';
+import { getDescription } from '../lib/descriptions';
 
 export default class DiceWrapper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             values: [],
-            valuesLoaded: false
+            valuesLoaded: false,
+            description: '',
+            descriptionShown: false
         };
         this.newGameButton = {
             light: 'border-sky-900 text-sky-900 hover:text-white hover:bg-sky-900',
@@ -21,9 +24,16 @@ export default class DiceWrapper extends React.Component {
             const max = Math.floor(7);
             newValues.push(Math.floor(Math.random() * (max - min) + min));
         }
+        
         window.setTimeout(() => {
             this.setState({ values: newValues, valuesLoaded: true });
-        }, 100);
+            if(this.props.specialMode) {
+                const description = getDescription(newValues, this.props.layedOutOnes, this.props.move);
+                window.setTimeout(() => {
+                    this.setState({ description: description, descriptionShown: true });
+                }, 200);
+            }
+        }, 200);
     }
 
     onOnesLayout(ones, sixes) {
@@ -47,9 +57,12 @@ export default class DiceWrapper extends React.Component {
         let sixes = this.state.values.filter(value => value === 6);
         const onesPresent = ones.length > 0 || sixes.length > 1;
         return(
-            <div className="w-full h-full">
+            <div className="w-full h-full relative">
+                <div className={ `${ this.state.descriptionShown ? 'visible scale-100' : 'invisible scale-50' } duration-200 transition-transform w-full flex justify-center absolute font-bold text-3xl sm:text-4xl text-center` }>
+                    { this.state.description }
+                </div>
                 <div className={ `${ this.state.valuesLoaded ? 'opacity-100' : 'opacity-0 translate-y-1' } w-full h-4/6 md:h-5/6 flex content-center gap-y-6 justify-center gap-x-6 flex-wrap mb-2 transition-all duration-200` }>
-                    { this.state.values.map((value, i) => <Dice key={i} value={value} diceType="big" theme={ this.props.theme }></Dice>) }
+                    { this.state.values.map((value, i) => <Dice key={ `bigDice${ i }` } value={value} diceType="big" theme={ this.props.theme }></Dice>) }
                 </div>
                 {this.props.move < 3 && ones.length < 3 && this.props.currentOnes + ones.length < 3
                     ? <div className="h-2/6 md:h-1/6 flex flex-col md:flex-row items-center justify-center gap-x-4 gap-y-4">
