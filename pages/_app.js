dc54cfcf40b5e90c39e63e04578b66d1155b374a
sobertrauter <robert_sauter@gmx.de>
2022-05-30 19:23:22 +0200

    changed to next pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
 import Head from 'next/head';
 import '../styles/globals.css';
-import { useEffect } from 'react';
-
-const registerServiceWorker = async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register('sw.js');
-      if (registration.installing) {
-        console.log('Service worker installing');
-      } else if (registration.waiting) {
-        console.log('Service worker installed');
-      } else if (registration.active) {
-        console.log('Service worker active');
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => { registerServiceWorker() });
-
   return(
     <>
       <Head>
---
+++ pages/_offline.js
@@ -0,0 +1,7 @@
+export default function Fallback(props) {
+    return(
+        <div className="flex items-center justify-center">
+            <p>The page could not be loaded, please try again, when you have an internet connection.</p>
+        </div>
+    );
+}
